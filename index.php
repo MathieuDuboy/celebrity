2e63b32b9315ec79270def1d78721214bfef1090
MathieuDuboy <11943636+MathieuDuboy@users.noreply.github.com>
2018-09-12 22:29:54 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-<?php
-// Insert de la page functions_wikipedia.php
-include("functions_wikipedia.php");
+<html>
+<head>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+</head>
+  <body>
+  <?php
+    // Insert de la page functions_wikipedia.php
+    include("functions_wikipedia.php");
 
-// Remplacer ici "Mylène_Farmer" par la variable contenant le titre de la page Wikipedia
-// Exemple : get_datas_from_wiki($titre_de_la_page_wikipedia);
-get_datas_from_wiki("Mylène_Farmer");
-?>
+    // Remplacer ici "Mylène_Farmer" par la variable contenant le titre de la page Wikipedia
+    // Exemple : get_datas_from_wiki($titre_de_la_page_wikipedia);
+    get_datas_from_wiki("Mylène_Farmer");
+  ?>
+  </body>
+</html>
